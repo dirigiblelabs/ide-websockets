a91cca530967543b368bb772752e427bc2212c7d
StanZGenchev <stan.z.genchev@gmail.com>
2022-11-29 17:30:52 +0200

    Migrated editor to the new view system

--- ide-websockets/services/websocket-editor.js
+++ ide-websockets/services/websocket-editor.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2010-2022 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -8,14 +8,16 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-exports.getEditor = function () {
-	return {
-		"id": "websocket",
-		"name": "Websocket",
-		"factory": "frame",
-		"region": "center-top",
-		"label": "Websocket",
-		"link": "../ide-websockets/editor/editor.html",
-		"contentTypes": ["application/json+websocket"]
-	};
+const editorData = {
+	id: "websocket",
+	factory: "frame",
+	region: "center-top",
+	label: "Websocket",
+	link: "../ide-websockets/editor/editor.html",
+	contentTypes: ["application/json+websocket"]
 };
+if (typeof exports !== 'undefined') {
+	exports.getEditor = function () {
+		return editorData;
+	}
+}
